775fab7d1c25187a1b4312c11312e6eb39dbac55
Rudra-bhatt <156334678+08-rudra@users.noreply.github.com>
2024-02-14 20:12:17 +0530

    DarkMode Added

--- script.js
+++ script.js
@@ -4,3 +4,46 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('section');
+
+    function isSectionFullyInView(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+
+    function toggleSectionAnimation() {
+      sections.forEach(section => {
+        if (isSectionFullyInView(section)) {
+          section.classList.add('in-view');
+        } else {
+          section.classList.remove('in-view');
+        }
+      });
+    }
+
+    // Listen for scroll events and toggle section animation accordingly
+    window.addEventListener('scroll', toggleSectionAnimation);
+    window.addEventListener('resize', toggleSectionAnimation);
+
+    // Trigger animation on page load
+    toggleSectionAnimation();
+  });
+
+  const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', function() {
+  if(this.checked) {
+    // Dark mode is enabled
+    document.body.classList.add('dark-mode');
+  } else {
+    // Dark mode is disabled
+    document.body.classList.remove('dark-mode');
+  }   
+});
